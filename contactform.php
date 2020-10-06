6fee8fe33053183c85912b09ee2324d5db520654
Henna Partanen <henna.partanen@itvelhot.local>
2020-10-06 12:22:24 +0300

    phpeetä

---
+++ contactform.php
@@ -0,0 +1,22 @@
+<?php 
+
+function laheta (){
+if (isset($_POST['nimi']) && isset($_POST['sahkoposti']) && isset($_POST['palaute'])) {
+
+    $data = 'palaute.txt' . $_POST['nimi'] . ' ' . $_POST['sahkoposti'] . ' ' . $_POST['palaute'] . "\n";
+    $ret = file_put_contents('palaute.txt', $data FILE_APPEND);
+  
+    if ($ret === false) {
+      echo('Ongelma palautetta lähettäessä');
+    } 
+  
+    else {
+      echo "Palaute on lähetetty!";
+    }
+   
+
+  }
+
+}
+
+?>
