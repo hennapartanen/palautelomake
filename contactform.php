7107c4b0afe2364ee0003ce5ec89f3a1f0e38101
Henna Partanen <henna.partanen@itvelhot.local>
2020-10-07 13:24:25 +0300

    kissa

--- contactform.php
+++ contactform.php
@@ -1,13 +1,17 @@
+
+
 <?php 
 
 
 if (isset($_POST['nimi']) && isset($_POST['sahkoposti']) && isset($_POST['palaute'])) {
 
-    $data = 'palaute.txt' . $_POST['nimi'] . ' ' . $_POST['sahkoposti'] . ' ' . $_POST['palaute'] . "\n";
-    $ret = file_put_contents('palaute.txt', $data FILE_APPEND);
-  
+   
+    $data = 'palautteet.html' . $_POST['nimi'] . ' ' . $_POST['sahkoposti'] . ' ' . $_POST['palaute'] . "\n";
+    $ret = file_put_contents('palautteet.html', $data, FILE_APPEND) ;
+   
+   
     if ($ret === false) {
-      echo('Ongelma palautetta lähettäessä');
+      echo "Ongelma palautetta lähettäessä";
     } 
   
     else {
@@ -20,3 +24,4 @@ if (isset($_POST['nimi']) && isset($_POST['sahkoposti']) && isset($_POST['palaut
 
 
 ?>
+
